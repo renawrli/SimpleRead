aefad7e3f67b311c7ef23784627d19a2c819fe52
renawrli <43318984+renawrli@users.noreply.github.com>
2018-11-04 01:01:09 -0500

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-function getText() {
-  var arr = document.getElementsByTagName("p");
-  var headerList = ["h1", "h2", "h3", "h4", "h6"];
-  for (var i = 0; i<headerList.length; i++) {
-    arr.concat(Array.from(document.getElementsByTagName(headerList[i])));
-  }
-}
